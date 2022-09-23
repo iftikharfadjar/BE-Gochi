22ad83a57f157c5c676a4d7958ccf8e14e440469
iftikharfadjar <42471332+iftikharfadjar@users.noreply.github.com>
2022-09-23 08:20:36 +0000

    initial router

--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"github.com/iftikharfadjar/Be-GoChi/router"
+)
+
+var (
+	httpRouter = router.NewChiRouter()
 )
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	const port string = ":3000"
+	httpRouter.SERVE(port)
 }
