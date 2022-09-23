a22fdaef472fa3b85d2640d436190b481c451911
iftikharfadjar <42471332+iftikharfadjar@users.noreply.github.com>
2022-09-23 09:52:18 +0000

    fix *chi.Mux

--- router/router.go
+++ router/router.go
@@ -2,14 +2,9 @@ package router
 
 import (
 	"github.com/go-chi/chi/v5"
-	"net/http"
 )
 
 type IRouter interface {
-	GET(uri string, f func(w http.ResponseWriter, r *http.Request))
-	POST(uri string, f func(w http.ResponseWriter, r *http.Request))
-	DELETE(uri string, f func(w http.ResponseWriter, r *http.Request))
-	PUT(uri string, f func(w http.ResponseWriter, r *http.Request))
 	SERVE(port string)
-	GetRouter() chi.Router
+	GetRouter() *chi.Mux
 }
