9a0a52f2dea934e8dc896002a9389aa062547601
iftikharfadjar <42471332+iftikharfadjar@users.noreply.github.com>
2022-11-30 11:31:21 +0000

    fail connect mongodb

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"time"
+
 	"github.com/iftikharfadjar/BE-Gochi/router"
 	_route "github.com/iftikharfadjar/BE-Gochi/routes"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var (
@@ -11,10 +16,35 @@ var (
 )
 
 func main() {
-	fmt.Println("Hello, World!")
-
 	const port string = ":3000"
 
+	// serverAPIOptions := options.ServerAPI(options.ServerAPIVersion1)
+	// clientOptions := options.Client().
+	// 	ApplyURI("mongodb+srv://admin:UXNIhGeK3Rp655Oi@fainacluster.bu653.mongodb.net/coursekita_db").
+	// 	SetServerAPIOptions(serverAPIOptions)
+	// ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	// defer cancel()
+	// client, err := mongo.Connect(ctx, clientOptions)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb+srv://admin:UXNIhGeK3Rp655Oi@fainacluster.bu653.mongodb.net/coursekita_db"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+
+
+
+
 	_route.InitRoute(httpRouter)
+
+	
+
+
 	httpRouter.SERVE(port)
 }
